8b16f7ce5e5e8cb8112d8e9ffab8eaa0a636b845
H4ZARD9 <122539729+H4ZARD9@users.noreply.github.com>
2023-01-31 10:18:10 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    P1 += 1
+    Rounds += 1
+    update_scoreboard()
+})
+function update_scoreboard () {
+    OLED.clear()
+    OLED.writeStringNewLine("\"player A: " + P1)
+    OLED.newLine()
+    OLED.writeStringNewLine("player B: " + P2)
+    OLED.newLine()
+    OLED.writeStringNewLine("ties: " + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("rounds: " + Rounds)
+}
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Tie")
+    Ties += 1
+    Rounds += 1
+    update_scoreboard()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    P2 += 1
+    Rounds += 1
+    update_scoreboard()
+})
 input.onGesture(Gesture.Shake, function () {
     reset()
 })
@@ -9,20 +37,10 @@ function reset () {
     Rounds = 0
     OLED.writeStringNewLine("shall we play a game")
     basic.pause(2000)
-    OLED.clear()
-    OLED.writeStringNewLine("\"player 1 score-" + P1)
-    OLED.newLine()
-    OLED.writeStringNewLine("player 2 score-" + P2)
-    OLED.newLine()
-    OLED.writeStringNewLine("ties-" + Ties)
-    OLED.newLine()
-    OLED.writeStringNewLine("rounds-" + Rounds)
+    update_scoreboard()
 }
-let Rounds = 0
 let Ties = 0
 let P2 = 0
+let Rounds = 0
 let P1 = 0
 reset()
-basic.forever(function () {
-	
-})
